f0fa96fab72aec7ff146ddfc193771fca30cf564
swinsider <swinsider@gmail.com>
2016-05-13 09:49:01 +0900

    good

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World???");
+            //Hello, Comment
+            Console.WriteLine("Hi!");
         }
     }
 }
